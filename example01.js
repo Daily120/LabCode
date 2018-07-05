903bb892ae9917cac684090169e0dc0d19a21bf1
Daily <forzhacker@mail.ri>
2018-07-05 12:29:44 +0200

    Changed the code to control both LED at the same time. Example 1/1 both on

--- example01.js
+++ example01.js
@@ -8,22 +8,28 @@ var board = new firmata.Board("/dev/ttyACM0", function() {
 
 http.createServer(function(req, res){
   var parts = req.url.split("/"),
-  operator= parseInt(parts[1],10);
+  operator1= parseInt(parts[1],10);
+  operator2 = parseInt(parts[2],10);
 
-  if(operator == 0) {
+  if(operator1 == 0 && operator2 == 0) {
     board.digitalWrite(13, board.LOW);
-  }
-  else if (operator == 1) {
-    board.digitalWrite(13, board.HIGH);
-  }
-  else if(operator == 2) {
     board.digitalWrite(12, board.LOW);
   }
-  else if (operator == 3) {
+  else if (operator1 == 1 && operator2 == 0) {
+    board.digitalWrite(13, board.HIGH);
+    board.digitalWrite(12, board.LOW);
+
+  }
+  else if(operator1 == 0 && operator2 == 1) {
+    board.digitalWrite(13, board.LOW);
+    board.digitalWrite(12, board.HIGH);
+  }
+  else if (operator1 == 1 && operator2 == 1) {
+    board.digitalWrite(13, board.HIGH);
     board.digitalWrite(12, board.HIGH);
   }
 
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.write("For test write into browser address line: http://192.168.1.134:8080/1 \n");
-  res.end("The value of operator: " + operator);
+  res.end("The value of operator: " + operator1 +" "+ operator2);
 }).listen(8080, "192.168.1.134");
