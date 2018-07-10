ffe4ebf1d7f6bc98219c793a0ba97b37efe7f409
Daily <forzhacker@mail.ri>
2018-07-10 12:44:28 +0200

    example03 is about controling 2 arduino(power on Pins):1) which is connected to another computer 2)which is connected to yours

---
+++ example03.js
@@ -0,0 +1,44 @@
+var http = require("http").createServer(handler);
+var io = require("socket.io").listen(http);
+var fs = require("fs");
+var firmata = require("firmata");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){
+
+    console.log("Connecting to Arduino");
+    console.log("Acctivation of Pin 13");
+    console.log("Acctivation of Pin 12");
+    board.pinMode(13, board.MODES.OUTPUT);
+    board.pinMode(12, board.MODES.OUTPUT);
+});
+
+function handler(req, res) {
+  fs.readFile(__dirname + "/example03.html",
+  function(err, data)  {
+    if (err) {
+        res.writeHea(500, {"Content-Type": "text/plain"});
+        return res.end("Error loading html page.");
+    }
+  res.writeHead(200);
+  res.end(data);
+});
+}
+
+http.listen(8080);
+
+io.sockets.on("connection", function(socket) {
+  socket.on("commandToArduino", function(commandNo) {
+    if (commandNo == "1") {
+        board.digitalWrite(13, board.HIGH);
+    }
+    if (commandNo == "0") {
+      board.digitalWrite(13, board.LOW);
+    }
+    if (commandNo == "3") {
+        board.digitalWrite(12, board.HIGH);
+    }
+    if (commandNo == "2") {
+      board.digitalWrite(12, board.LOW);
+    }
+  });
+});
