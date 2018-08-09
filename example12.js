511e04f59a0088d1b1249dd17a66311d5e5a0669
Daily <forzhacker@mail.ri>
2018-08-09 11:27:22 +0200

    Adding buttons to start and stop motor position control algorithm + log improvement

--- example12.js
+++ example12.js
@@ -2,6 +2,7 @@ var http = require("http").createServer(handler);
 var io = require("socket.io").listen(http);
 var fs = require("fs");
 var firmata = require("firmata");
+var pwm = 0; //for logging the speed of DC-Motor
 
 var board = new firmata.Board("/dev/ttyACM0", function(){
   console.log("Enabling analog Pin 0");
@@ -31,13 +32,19 @@ http.listen(8080);
 
 io.sockets.on("connection", function(socket) {
   board.analogRead(0, function(value){
-    desiredValue = value; // continuous read of analog pin 0
+      desiredValue = value; // continuous read of analog pin 0
   });
   board.analogRead(1, function(value) {
-    actualValue = value; // continuous read of pin A1
+      actualValue = value; // continuous read of pin A1
   });
 
-  startControlAlgorithm();
+  socket.on("startControlAlgorithm", function(){
+      startControlAlgorithm();
+  });
+
+  socket.on("stopControlAlgorithm", function(){
+      stopControlAlgorithm();
+  });
 
   socket.emit("messageToClient", "Server connected, board ready.");
   setInterval(sendValues, 40, socket); // na 40ms we send message to client
@@ -47,12 +54,15 @@ function sendValues (socket) {
   socket.emit("clientReadValues",
   {
     "desiredValue": desiredValue,
-    "actualValue": actualValue
+    "actualValue": actualValue,
+    "pwm": pwm
   });
 };
 
 //DC-Motor Position Algorithm
 var factor = 1.25; // proportional factor that determines the speed of aproaching toward desired value
+var controlAlgorihtmStartedFlag = 0; // flag in global scope to see weather ctrlAlg has been started
+var intervalCtrl; // var for setInterval in global space
 
 function controlAlgorithm () {
     pwm = factor*(desiredValue-actualValue);
@@ -64,6 +74,15 @@ function controlAlgorithm () {
 };
 
 function startControlAlgorithm () {
-    setInterval(function() {controlAlgorithm(); }, 30); // call every 30ms
-    console.log("Control algorithm started")
+    if (controlAlgorihtmStartedFlag == 0) {
+        controlAlgorihtmStartedFlag = 1; // set flag that the algorithm has started
+        intervalCtrl = setInterval(function() {controlAlgorithm(); }, 30);
+        console.log("Control algorithm started");
+    }
+};
+
+function stopControlAlgorithm () {
+    clearInterval(intervalCtrl); // clear the interval of control algorihtm
+    board.analogWrite(3,0); // write 0 on pwm pin to stop the motor
+    controlAlgorihtmStartedFlag = 0; // set flag that the algorithm has stopped
 };
