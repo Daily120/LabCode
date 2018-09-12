085aca51a1b21b8a6a1b4db235ff442e19e29d18
Daily120 <forzhacker@mail.ru>
2018-09-12 14:10:36 +0200

    Additional graph for Fuzzy controller

--- example16.js
+++ example16.js
@@ -31,6 +31,15 @@ var desiredValue = 0; // desired value var
 var actualValue = 0; // actual value var
 
 var pwm = 0; // set pwm as global variable
+var pwmLimit = 254; // to limit value of the pwm that is sent to the motor
+
+var err = 0; // error
+var errSum = 0; // sum of errors as integral
+var dErr = 0; // difference of error
+var lastErr = 0; // to keep the value of previous error to estimate derivative
+
+var controlAlgorithmStartedFlag = 0; // variable for indicating weather the Alg has benn sta.
+var intervalCtrl; // var for setInterval in global scope
 
 http.listen(8080); // server will listen on port 8080
 
@@ -44,12 +53,23 @@ board.on("ready", function () {
         actualValue = value; // continuous read of analog pin 1
     });
 
-    startControlAlgorithm();
-
     io.sockets.on("connection", function (socket) {
         socket.emit("messageToClient", "Srv connected, board OK");
+        socket.emit("staticMsgToClient", "Srv connected, board OK"); // ??????
 
         setInterval(sendValues, 40, socket); // on 40ms trigerr func. sendValues
+
+        socket.on("startControlAlgorithm", function(numberOfControlAlgorithm){
+            startControlAlgorithm(numberOfControlAlgorithm); 
+         });
+         
+        socket.on("stopControlAlgorithm", function(){
+            stopControlAlgorithm(); 
+         });
+         
+        sendStaticMsgViaSocket = function(value) {
+            io.sockets.emit("staticMsgToClient", value);  
+         };
 
     }); // end of sockets.on connection
 
@@ -93,30 +113,34 @@ RBase[RN][0] = 2;
 RBase[RN][1] = 2;
 RN++;
 
-FSvalues[0][0][0] = -100; // number of var, num of fuzzy sets, number of points, that define fs
-FSvalues[0][0][1] = -100;
-FSvalues[0][0][2] = 0;
+var FSpos1 = 125;
+var FSpos2 = 25;
 
-FSvalues[0][1][0] = -100;
-FSvalues[0][1][1] = 0;
-FSvalues[0][1][2] = 100;
-
-FSvalues[0][2][0] = 0;
-FSvalues[0][2][1] = 100;
-FSvalues[0][2][2] = 100;
-
-FSvalues[1][0][0] = -15;
-FSvalues[1][0][1] = -15;
-FSvalues[1][0][2] = 0;
-
-FSvalues[1][1][0] = -15;
-FSvalues[1][1][1] = 0;
-FSvalues[1][1][2] = 15;
-
-FSvalues[1][2][0] = 0;
-FSvalues[1][2][1] = 15;
-FSvalues[1][2][2] = 15;
-
+function resetFSvalues() {
+    FSvalues[0][0][0] =  -FSpos1;
+    FSvalues[0][0][1] =  -FSpos1;
+    FSvalues[0][0][2] =  0;    
+    
+    FSvalues[0][1][0] =  -FSpos1;
+    FSvalues[0][1][1] =  0;
+    FSvalues[0][1][2] =  FSpos1;
+    
+    FSvalues[0][2][0] =  0;
+    FSvalues[0][2][1] =  FSpos1;
+    FSvalues[0][2][2] =  FSpos1;
+    
+    FSvalues[1][0][0] =  -FSpos2;
+    FSvalues[1][0][1] =  -FSpos2;
+    FSvalues[1][0][2] =  0;    
+    
+    FSvalues[1][1][0] =  -FSpos2;
+    FSvalues[1][1][1] =  0;
+    FSvalues[1][1][2] =  FSpos2;
+    
+    FSvalues[1][2][0] =  0;
+    FSvalues[1][2][1] =  FSpos2;
+    FSvalues[1][2][2] =  FSpos2;
+}
 
 function getMRFromFSvalues(value, NumOfVar, NumOfSet) {
     if (NumOfSet === 0) {
@@ -127,7 +151,7 @@ function getMRFromFSvalues(value, NumOfVar, NumOfSet) {
         } else {
             return (FSvalues[NumOfVar][NumOfSet][2] - value) / (FSvalues[NumOfVar][NumOfSet][2] - FSvalues[NumOfVar][NumOfSet][1]);
         }
-    } else if (NumOfSet == NFuzzySets[NumOfVar] - 1) {
+    } else if (NumOfSet === NFuzzySets[NumOfVar] - 1) {
         if (value < FSvalues[NumOfVar][NumOfSet][0]) {
             return 0;
         } else if (value > FSvalues[NumOfVar][NumOfSet][2]) {
@@ -149,6 +173,7 @@ function getMRFromFSvalues(value, NumOfVar, NumOfSet) {
 }
 
 function getDesiredValuesFuzzy() {
+    resetFSvalues();
     for (var j = 0; j != NFuzzySets[0]; j++) {
         AlphaCut[j] = 0;
     }
@@ -169,6 +194,7 @@ function getDesiredValuesFuzzy() {
 
     for (var CurrentVar = 0; CurrentVar != NFuzzyOutputs; CurrentVar++) {
         var FuzzyRange = FSvalues[CurrentVar][NFuzzySets[CurrentVar] - 1][2] - FSvalues[CurrentVar][0][0];
+        console.log(FuzzyRange);
         var NIntervals = 100;
         var CoordinateMassSumm = 0;
         var MassSumm = 0;
@@ -194,34 +220,86 @@ function getDesiredValuesFuzzy() {
 }
 ////////////////////////////////////////////////unfuzzy
 
-function controlAlgorithm() {
-
-    ValuesForFuzzy[1] = desiredValue - actualValue;
-    console.log(ValuesForFuzzy[1]);
-    getDesiredValuesFuzzy();
-    pwm = ValuesForFuzzy[0];
-    console.log(pwm);
-    if (pwm > 255) { pwm = 255 };
-    if (pwm < -255) { pwm = -255 };
-    if (pwm > 0) {
-        board.digitalWrite(2, 0);
-    }; // direction if > 0
-    if (pwm < 0) {
-        board.digitalWrite(2, 1);
-    }; // direction if < 0
+function sendPwmToArduino() {
+    if (pwm > pwmLimit) {pwm = pwmLimit} // to limit pwm values
+    if (pwm < -pwmLimit) {pwm = -pwmLimit} // to limit pwm values
+    if (pwm > 0) {board.digitalWrite(2, 0)} // direction if > 0
+    if (pwm < 0) {board.digitalWrite(2, 1)} // direction if < 0
     board.analogWrite(3, Math.abs(pwm));
+}
+
+function controlAlgorithm (parameters) {
+    switch (parameters.ctrlAlgNo) {
+        case 1:
+            pwm = parameters.pCoeff * (desiredValue - actualValue);
+            sendPwmToArduino();
+            break;
+        case 2:
+            err = desiredValue - actualValue; // error as difference between desired and actual val.
+            errSum += err; // sum of errors | like integral
+            dErr = err - lastErr; // difference of error
+            pwm = parameters.Kp1 * err + parameters.Ki1 * errSum + parameters.Kd1 * dErr; // PID expression
+            lastErr = err; // save the value of error for next cycle to estimate the derivative
+            sendPwmToArduino();
+            break;
+        case 3:
+            err = desiredValue - actualValue; // error as difference between desired and actual val.
+            errSum += err; // sum of errors | like integral
+            dErr = err - lastErr; // difference of error
+            pwm = parameters.Kp2 * err + parameters.Ki2 * errSum + parameters.Kd2 * dErr; // PID expression
+            console.log(parameters.Kp2 + "|" + parameters.Ki2 + "|" + parameters.Kd2);
+            lastErr = err; // save the value of error for next cycle to estimate the derivative
+            sendPwmToArduino();
+            break;
+        case 4:
+            ValuesForFuzzy[1] = desiredValue - actualValue;
+            FSpos1 = parameters.FSpos1;
+            FSpos2 = parameters.FSpos2;
+            //console.log(parameters);
+            getDesiredValuesFuzzy();
+            pwm = ValuesForFuzzy[0];
+            err = desiredValue - actualValue; // error as difference between desired and actual val.
+            
+            lastErr = err; // save the value of error for next cycle to estimate the derivative
+            sendPwmToArduino();
+            break;
+    }
 };
 
-function startControlAlgorithm() {
-    setInterval(function () {
-        controlAlgorithm();
-    }, 30); // call the alg. on 30ms
-    console.log("Control algorithm has been started.");
+function startControlAlgorithm (parameters) {
+    if (controlAlgorithmStartedFlag === 0) {
+        controlAlgorithmStartedFlag = 1;
+        intervalCtrl = setInterval(function(){controlAlgorithm(parameters);}, 30); // call the alg. on 30ms
+        console.log("Control algorithm has been started.");
+        sendStaticMsgViaSocket("Control alg " + parameters.ctrlAlgNo + " started | " + JSON.stringify(parameters));
+    }
+ 
 };
-
-function sendValues(socket) {
-    socket.emit("clientReadValues", {
-        "desiredValue": desiredValue,
-        "actualValue": actualValue
+ 
+function stopControlAlgorithm () {
+    clearInterval(intervalCtrl); // clear the interval of control algorihtm
+    board.analogWrite(3, 0);
+    err = 0; // error as difference between desired and actual val.
+    errSum = 0; // sum of errors | like integral
+    dErr = 0;
+    lastErr = 0; // difference
+    pwm = 0;
+    
+    controlAlgorithmStartedFlag = 0;
+    console.log("Control algorithm has been stopped.");
+    sendStaticMsgViaSocket("Stopped.")
+};
+ 
+function sendValues (socket) {
+    socket.emit("clientReadValues",
+    {
+    "desiredValue": desiredValue,
+    "actualValue": actualValue,
+    "pwm": pwm,
+    "err": err,
+    "errSum": errSum,
+    "dErr": dErr,
+    "AlphaCut":AlphaCut,
+    "ValuesForFuzzy":ValuesForFuzzy
     });
 };
